bfa8054426ea8f9c147476012de415361e278854
Dharshini151 <108514100+Dharshini151@users.noreply.github.com>
2023-04-05 10:21:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FetchNews from './Component/Fetch';
+
+function App() {
+  return (
+    <div className="App">
+      <FetchNews/>
+    </div>
+  );
+}
+
+export default App;
